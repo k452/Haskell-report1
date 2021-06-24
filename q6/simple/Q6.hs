3fa7c42a2cf95548abd2a75fe48b494f084b5075
k452 <55301839+k452@users.noreply.github.com>
2021-06-25 01:31:12 +0900

    cata用にディレクトリ

---
+++ q6/simple/Q6.hs
@@ -0,0 +1,24 @@
+module Q6 where
+
+data BETree a = BELeaf a | Nil | BENode a (BETree a) (BETree a) deriving (Eq, Show)
+
+depthBETree :: (Ord a, Num a, Eq a) => BETree a -> a
+depthBETree (BELeaf a) = 0
+depthBETree Nil = 0
+depthBETree (BENode a lt rt)
+  | lt == Nil = 1 + depthBETree rt
+  | rt == Nil = 1 + depthBETree lt
+  | otherwise = 1 + max (depthBETree lt) (depthBETree rt)
+
+sumBETree :: (Num a, Eq a) => BETree a -> a
+sumBETree (BELeaf a) = a
+sumBETree Nil = 0
+sumBETree (BENode a lt rt)
+  | lt == Nil = a + sumBETree rt
+  | rt == Nil = a + sumBETree lt
+  | otherwise = a + sumBETree lt + sumBETree rt
+
+upAccBETree :: (Num a, Eq a) => BETree a -> BETree a
+upAccBETree (BELeaf a) = BELeaf a
+upAccBETree Nil = Nil
+upAccBETree (BENode a lt rt) = BENode (sumBETree (BENode a lt rt)) (upAccBETree lt) (upAccBETree rt)
---
+++ q6/simple/TestSumBTree.hs
@@ -0,0 +1,68 @@
+import Q6 (BETree (BELeaf, BENode, Nil), sumBETree)
+import Test.HUnit
+  ( Assertable (assert),
+    Counts,
+    Test (TestCase, TestLabel),
+    Testable (test),
+    assertEqual,
+    runTestTT,
+  )
+
+b1 :: BETree Integer
+b1 = BELeaf 1
+
+case1 :: Test
+case1 =
+  TestCase
+    ( assertEqual
+        "葉1枚"
+        (sumBETree b1)
+        1
+    )
+
+b2 :: BETree Integer
+b2 = BENode 1 (BELeaf 2) Nil
+
+case2 :: Test
+case2 =
+  TestCase
+    ( assertEqual
+        "葉2枚"
+        (sumBETree b2)
+        3
+    )
+
+b3 :: BETree Integer
+b3 = BENode 1 (BENode 2 (BENode 3 (BELeaf 4) Nil) (BENode 5 (BELeaf 6) (BELeaf 7))) (BELeaf 8)
+
+case3 :: Test
+case3 =
+  TestCase
+    ( assertEqual
+        "複雑1"
+        (sumBETree b3)
+        36
+    )
+
+b4 :: BETree Integer
+b4 = BENode 5 (BENode 8 (BELeaf 3) (BENode 1 (BELeaf 7) Nil)) (BENode 6 (BENode 2 Nil (BELeaf 9)) (BELeaf 4))
+
+case4 :: Test
+case4 =
+  TestCase
+    ( assertEqual
+        "プリントの例"
+        (sumBETree b4)
+        45
+    )
+
+main :: IO Test.HUnit.Counts
+main =
+  runTestTT
+    ( test
+        [ TestLabel "case1" case1,
+          TestLabel "case2" case2,
+          TestLabel "case3" case3,
+          TestLabel "case4" case4
+        ]
+    )
