62d6c3feead01e8150bcfb235a707ed670b25df2
k452 <55301839+k452@users.noreply.github.com>
2021-06-27 23:07:11 +0900

    otherwise

--- q1/Q1.hs
+++ q1/Q1.hs
@@ -5,18 +5,18 @@ myTake _ [] = []
 myTake n all@(x : xs)
   | n > length all || n == length all = all
   | n == 0 = []
-  | n < length all = x : myTake (n -1) xs
+  | otherwise = x : myTake (n -1) xs
 
 myTakeWhile :: (a -> Bool) -> [a] -> [a]
 myTakeWhile _ [] = []
 myTakeWhile f all@(x : xs)
   | null all = []
   | f x = x : myTakeWhile f xs
-  | not (f x) = []
+  | otherwise = []
 
 myDropWhile :: (a -> Bool) -> [a] -> [a]
 myDropWhile _ [] = []
 myDropWhile f all@(x : xs)
   | null all = []
   | f x = myDropWhile f xs
-  | not (f x) = all
+  | otherwise = all
